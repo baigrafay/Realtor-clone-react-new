aeb42465e83fbb13a0a734cc57b7d92e7c96c2fd
rafay baig <rafay@ENG-KICT.kictl.com>
2023-04-13 04:23:04 +0500

    complete the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import {getAuth, onAuthStateChanged} from "firebase/auth"
 export default function Header() {
+  const [pageState,setPageState]=useState("Sign In")
   const location = useLocation();
   const navigate = useNavigate();
+  const auth =getAuth();
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+        setPageState("Profile")
+      }else{
+        setPageState("Sign In");
+      }
+    })
+
+  },[auth])
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
@@ -39,11 +53,11 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMatchRoute("/sign-in") &&"text-black border-b-red-500"
+              ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&"text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
