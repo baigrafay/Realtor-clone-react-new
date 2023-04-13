10a2cc29ae38e147eda80d5e0212598dd220773e
rafay baig <rafay@ENG-KICT.kictl.com>
2023-04-13 18:49:44 +0500

    UI of create lisitng page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import CreateListing from "./pages/CreateListing";
 function App() {
   return (
     <>
@@ -18,10 +19,11 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/profile"element={<PrivateRoute/>}>
             <Route path="/profile" element={<Profile/>}/>
-
           </Route>
-          
           <Route path="/offers" element={<Offers/>}/>
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
+import {FcHome} from "react-icons/fc"
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const auth=getAuth()
@@ -75,6 +77,14 @@ export default function Profile() {
                 <p className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer ' onClick={onLogout}>Sign Out</p>
             </div>
           </form>
+          <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 '>
+            <Link to="/create-listing" className='flex justify-center items-center '>
+              <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
+              Sell Or Rent Your Home 
+
+            </Link>
+            
+          </button>
         </div>
       </section>
     </>
