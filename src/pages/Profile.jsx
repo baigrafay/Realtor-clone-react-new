9b36f10df158690159d20be74b56e044e2264bde
rafay baig <rafay@ENG-KICT.kictl.com>
2023-04-14 00:08:49 +0500

    create my listing section to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,21 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { doc, updateDoc } from 'firebase/firestore';
-import React, { useState } from 'react'
+import { collection, doc, getDocs, orderBy, query, updateDoc,where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase';
 import {FcHome} from "react-icons/fc"
 import { Link } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
+
+
 
 export default function Profile() {
   const auth=getAuth()
   const navigate =useNavigate()
   const [changeDetail,setChangeDetail]=useState(false)
+  const [listings,setListings]=useState(null);
+  const [loading,setLoading]=useState(true);
   const [formData,setFormData]=useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -48,6 +53,27 @@ export default function Profile() {
     }
 
   }
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   
   return (
     <>
@@ -87,6 +113,21 @@ export default function Profile() {
           </button>
         </div>
       </section>
+      <div className='max-w-6xl px-3 mt-6 mx-auto '>
+        {!loading && listings.length >0 &&(
+          <>
+          <h2 className='text-2xl text-center font-semibold '>
+            My Listings
+          </h2>
+          <ul>
+            {listings.map((listing)=>(
+              <ListingItem key={listing.id} id={listing.id} listing={listing.data}
+              />
+            ))}
+          </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
